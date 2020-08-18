e4ef250c0ee4849a2a5a83e2695d1ca299df0982
jsch <jsch@jsch.Speedport_W_724V_01011603_06_003>
2020-08-18 19:01:52 +0200

    fixed type

--- src/cpu.hpp
+++ src/cpu.hpp
@@ -376,7 +376,7 @@ namespace gbaemu
                 overflow = state.getFlag(cpsr_flags::V_FLAG_BITMASK),
                 carry = state.getFlag(cpsr_flags::C_FLAG_BITMASK);
 
-            uint32_t rnValue = state.accessReg(inst.params.block_data_transf.rn);
+            uint64_t rnValue = state.accessReg(inst.params.block_data_transf.rn);
             uint64_t resultValue;
 
             /* Different instructions cause different flags to be changed. */
