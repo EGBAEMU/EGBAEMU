8ce19672afcd39ed0f2dc5648a352ba24f089d54
jsch <jsch@jsch.Speedport_W_724V_01011603_06_003>
2020-08-24 21:38:08 +0200

    hopefully resizeable

--- src/lcd/window.cpp
+++ src/lcd/window.cpp
@@ -6,8 +6,8 @@
 namespace gbaemu::lcd {
     Window::Window(uint32_t width, uint32_t height, const char *title) {
         SDL_Init(SDL_INIT_VIDEO);
-        window = SDL_CreateWindow(title, 0, 0,
-                                    width, height, SDL_WINDOW_SHOWN);
+        window = SDL_CreateWindow(title, 100, 100,
+            width, height, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
         assert(window);
         surface = SDL_GetWindowSurface(window);
         assert(surface);
