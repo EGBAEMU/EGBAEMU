0b116f5d3f3824ed415681240822cbd55814bee0
Tammo Mürmann <tammo.muermann@stud.tu-darmstadt.de>
2020-08-28 23:58:46 +0200

    arm inst toString use switch statement & add output for BX

--- src/inst_arm.cpp
+++ src/inst_arm.cpp
@@ -65,143 +65,168 @@ namespace gbaemu
         {
             std::stringstream ss;
 
-            ss << '(' << conditionCodeToString(condition) << ") ";
+            ss << '(' << conditionCodeToString(condition) << ") " << instructionIDToString(id);
 
-            if (cat == ARMInstructionCategory::DATA_PROC_PSR_TRANSF) {
-                /* TODO: probably not done */
-                bool hasRN = !(id == ARMInstructionID::MOV || id == ARMInstructionID::MVN);
-                bool hasRD = !(id == ARMInstructionID::TST || id == ARMInstructionID::TEQ ||
-                               id == ARMInstructionID::CMP || id == ARMInstructionID::CMN);
+            switch (cat) {
 
-                auto idStr = instructionIDToString(id);
-                uint32_t rd = params.data_proc_psr_transf.rd;
-                uint32_t rn = params.data_proc_psr_transf.rn;
-                ShiftType shiftType;
-                uint8_t shiftAmount;
-                uint32_t rm;
-                uint32_t rs;
-                uint32_t imm;
-                bool shiftByReg = params.data_proc_psr_transf.extractOperand2(shiftType, shiftAmount, rm, rs, imm);
+                case ARMInstructionCategory::DATA_PROC_PSR_TRANSF: {
+                    /* TODO: probably not done */
+                    bool hasRN = !(id == ARMInstructionID::MOV || id == ARMInstructionID::MVN);
+                    bool hasRD = !(id == ARMInstructionID::TST || id == ARMInstructionID::TEQ ||
+                                   id == ARMInstructionID::CMP || id == ARMInstructionID::CMN);
 
-                ss << idStr;
+                    uint32_t rd = params.data_proc_psr_transf.rd;
+                    uint32_t rn = params.data_proc_psr_transf.rn;
+                    ShiftType shiftType;
+                    uint8_t shiftAmount;
+                    uint32_t rm;
+                    uint32_t rs;
+                    uint32_t imm;
+                    bool shiftByReg = params.data_proc_psr_transf.extractOperand2(shiftType, shiftAmount, rm, rs, imm);
 
-                if (id == MSR) {
-                    // true iff write to flag field is allowed 31-24
-                    bool f = params.data_proc_psr_transf.rn & 0x08;
-                    // true iff write to status field is allowed 23-16
-                    bool s = params.data_proc_psr_transf.rn & 0x04;
-                    // true iff write to extension field is allowed 15-8
-                    bool x = params.data_proc_psr_transf.rn & 0x02;
-                    // true iff write to control field is allowed 7-0
-                    bool c = params.data_proc_psr_transf.rn & 0x01;
-                    ss << (params.data_proc_psr_transf.r ? " SPSR_" : " CPSR_");
-                    ss << (f ? "f" : "");
-                    ss << (s ? "s" : "");
-                    ss << (x ? "x" : "");
-                    ss << (c ? "c" : "");
-                    if (params.data_proc_psr_transf.i) {
-                        uint32_t roredImm = shift(imm, shiftType, shiftAmount, false, false);
-                        ss << ", #" << roredImm;
+                    if (id == MSR) {
+                        // true iff write to flag field is allowed 31-24
+                        bool f = params.data_proc_psr_transf.rn & 0x08;
+                        // true iff write to status field is allowed 23-16
+                        bool s = params.data_proc_psr_transf.rn & 0x04;
+                        // true iff write to extension field is allowed 15-8
+                        bool x = params.data_proc_psr_transf.rn & 0x02;
+                        // true iff write to control field is allowed 7-0
+                        bool c = params.data_proc_psr_transf.rn & 0x01;
+                        ss << (params.data_proc_psr_transf.r ? " SPSR_" : " CPSR_");
+                        ss << (f ? "f" : "");
+                        ss << (s ? "s" : "");
+                        ss << (x ? "x" : "");
+                        ss << (c ? "c" : "");
+                        if (params.data_proc_psr_transf.i) {
+                            uint32_t roredImm = shift(imm, shiftType, shiftAmount, false, false);
+                            ss << ", #" << roredImm;
+                        } else {
+                            ss << ", r" << std::dec << rm;
+                        }
                     } else {
-                        ss << ", r" << std::dec << rm;
+                        if (params.data_proc_psr_transf.s)
+                            ss << "{S}";
+
+                        if (hasRD)
+                            ss << " r" << rd;
+
+                        if (hasRN)
+                            ss << " r" << rn;
+
+                        if (params.data_proc_psr_transf.i) {
+                            uint32_t roredImm = shift(imm, shiftType, shiftAmount, false, false);
+                            ss << ", #" << roredImm;
+                        } else {
+                            ss << " r" << rm;
+
+                            if (shiftByReg)
+                                ss << "<<r" << std::dec << rs;
+                            else if (shiftAmount > 0)
+                                ss << "<<" << std::dec << static_cast<uint32_t>(shiftAmount);
+                        }
                     }
-                } else {
-                    if (params.data_proc_psr_transf.s)
+                    break;
+                }
+                case ARMInstructionCategory::MUL_ACC: {
+                    if (params.mul_acc.s)
                         ss << "{S}";
+                    ss << " r" << params.mul_acc.rd << " r" << params.mul_acc.rm << " r" << params.mul_acc.rs;
+                    if (params.mul_acc.a)
+                        ss << " +r" << params.mul_acc.rn;
+                    break;
+                }
+                case ARMInstructionCategory::MUL_ACC_LONG: {
+                    if (params.mul_acc_long.s)
+                        ss << "{S}";
+                    ss << " r" << params.mul_acc_long.rd_msw << ":r" << params.mul_acc_long.rd_lsw << " r" << params.mul_acc_long.rs << " r" << params.mul_acc_long.rm;
+                    break;
+                }
+                case ARMInstructionCategory::HW_TRANSF_REG_OFF: {
+                    /* No immediate in this category! */
+                    ss << " r" << params.hw_transf_reg_off.rd;
 
-                    if (hasRD)
-                        ss << " r" << rd;
-
-                    if (hasRN)
-                        ss << " r" << rn;
-
-                    if (params.data_proc_psr_transf.i) {
-                        uint32_t roredImm = shift(imm, shiftType, shiftAmount, false, false);
-                        ss << ", #" << roredImm;
+                    /* TODO: does p mean pre? */
+                    if (params.hw_transf_reg_off.p) {
+                        ss << " [r" << params.hw_transf_reg_off.rn << "+r" << params.hw_transf_reg_off.rm << ']';
                     } else {
-                        ss << " r" << rm;
-
-                        if (shiftByReg)
-                            ss << "<<r" << std::dec << rs;
-                        else if (shiftAmount > 0)
-                            ss << "<<" << std::dec << static_cast<uint32_t>(shiftAmount);
+                        ss << " [r" << params.hw_transf_reg_off.rn << "]+r" << params.hw_transf_reg_off.rm;
                     }
+                    break;
                 }
-            } else if (cat == ARMInstructionCategory::MUL_ACC) {
-                ss << instructionIDToString(id);
-                if (params.mul_acc.s)
-                    ss << "{S}";
-                ss << " r" << params.mul_acc.rd << " r" << params.mul_acc.rm << " r" << params.mul_acc.rs;
-                if (params.mul_acc.a)
-                    ss << " +r" << params.mul_acc.rn;
-            } else if (cat == ARMInstructionCategory::MUL_ACC_LONG) {
-                ss << instructionIDToString(id);
-                if (params.mul_acc_long.s)
-                    ss << "{S}";
-                ss << " r" << params.mul_acc_long.rd_msw << ":r" << params.mul_acc_long.rd_lsw << " r" << params.mul_acc_long.rs << " r" << params.mul_acc_long.rm;
-            } else if (cat == ARMInstructionCategory::HW_TRANSF_REG_OFF) {
-                /* No immediate in this category! */
-                ss << instructionIDToString(id) << " r" << params.hw_transf_reg_off.rd;
+                case ARMInstructionCategory::HW_TRANSF_IMM_OFF: {
+                    /* Immediate in this category! */
+                    ss << " r" << params.hw_transf_imm_off.rd;
 
-                /* TODO: does p mean pre? */
-                if (params.hw_transf_reg_off.p) {
-                    ss << " [r" << params.hw_transf_reg_off.rn << "+r" << params.hw_transf_reg_off.rm << ']';
-                } else {
-                    ss << " [r" << params.hw_transf_reg_off.rn << "]+r" << params.hw_transf_reg_off.rm;
+                    if (params.hw_transf_reg_off.p) {
+                        ss << " [r" << params.hw_transf_imm_off.rn << "+0x" << std::hex << params.hw_transf_imm_off.offset << ']';
+                    } else {
+                        ss << " [[r" << params.hw_transf_imm_off.rn << "]+0x" << std::hex << params.hw_transf_imm_off.offset << ']';
+                    }
+                    break;
                 }
-            } else if (cat == ARMInstructionCategory::HW_TRANSF_IMM_OFF) {
-                /* Immediate in this category! */
-                ss << instructionIDToString(id) << " r" << params.hw_transf_imm_off.rd;
+                case ARMInstructionCategory::LS_REG_UBYTE: {
+                    bool pre = params.ls_reg_ubyte.p;
+                    char upDown = params.ls_reg_ubyte.u ? '+' : '-';
 
-                if (params.hw_transf_reg_off.p) {
-                    ss << " [r" << params.hw_transf_imm_off.rn << "+0x" << std::hex << params.hw_transf_imm_off.offset << ']';
-                } else {
-                    ss << " [[r" << params.hw_transf_imm_off.rn << "]+0x" << std::hex << params.hw_transf_imm_off.offset << ']';
+                    ss << " r" << params.ls_reg_ubyte.rd;
+
+                    if (!params.ls_reg_ubyte.i) {
+                        uint32_t immOff = params.ls_reg_ubyte.addrMode & 0xFFF;
+
+                        if (pre)
+                            ss << " [r" << params.ls_reg_ubyte.rn;
+                        else
+                            ss << " [[r" << params.ls_reg_ubyte.rn << ']';
+
+                        ss << upDown << "0x" << std::hex << immOff << ']';
+                    } else {
+                        uint32_t shiftAmount = (params.ls_reg_ubyte.addrMode >> 7) & 0xF;
+                        uint32_t rm = params.ls_reg_ubyte.addrMode & 0xF;
+
+                        if (pre)
+                            ss << " [r" << params.ls_reg_ubyte.rn;
+                        else
+                            ss << " [[r" << params.ls_reg_ubyte.rn << ']';
+
+                        ss << upDown << "(r" << rm << "<<" << shiftAmount << ")]";
+                    }
+                    break;
                 }
-            } else if (cat == ARMInstructionCategory::LS_REG_UBYTE) {
-                bool pre = params.ls_reg_ubyte.p;
-                char upDown = params.ls_reg_ubyte.u ? '+' : '-';
+                case ARMInstructionCategory::BLOCK_DATA_TRANSF: {
+                    ss << " r" << params.block_data_transf.rn << " { ";
 
-                ss << instructionIDToString(id) << " r" << params.ls_reg_ubyte.rd;
+                    for (uint32_t i = 0; i < 16; ++i)
+                        if (params.block_data_transf.rList & (1 << i))
+                            ss << "r" << i << ' ';
 
-                if (!params.ls_reg_ubyte.i) {
-                    uint32_t immOff = params.ls_reg_ubyte.addrMode & 0xFFF;
-
-                    if (pre)
-                        ss << " [r" << params.ls_reg_ubyte.rn;
-                    else
-                        ss << " [[r" << params.ls_reg_ubyte.rn << ']';
-
-                    ss << upDown << "0x" << std::hex << immOff << ']';
-                } else {
-                    uint32_t shiftAmount = (params.ls_reg_ubyte.addrMode >> 7) & 0xF;
-                    uint32_t rm = params.ls_reg_ubyte.addrMode & 0xF;
-
-                    if (pre)
-                        ss << " [r" << params.ls_reg_ubyte.rn;
-                    else
-                        ss << " [[r" << params.ls_reg_ubyte.rn << ']';
-
-                    ss << upDown << "(r" << rm << "<<" << shiftAmount << ")]";
+                    ss << '}';
+                    break;
                 }
-            } else if (cat == ARMInstructionCategory::BLOCK_DATA_TRANSF) {
-                ss << instructionIDToString(id) << " r" << params.block_data_transf.rn << " { ";
+                case ARMInstructionCategory::BRANCH: {
+                    /*  */
+                    int32_t off = params.branch.offset * 4;
 
-                for (uint32_t i = 0; i < 16; ++i)
-                    if (params.block_data_transf.rList & (1 << i))
-                        ss << "r" << i << ' ';
+                    ss << (params.branch.l ? "L" : "") << " "
+                       << "PC" << (off < 0 ? '-' : '+') << "0x" << std::hex << std::abs(off);
+                    break;
+                }
+                case ARMInstructionCategory::SOFTWARE_INTERRUPT: {
+                    ss << " " << swi::swiToString(params.software_interrupt.comment >> 16);
+                    break;
+                }
+                case ARMInstructionCategory::BRANCH_XCHG: {
+                    ss << " r" << params.branch_xchg.rn;
+                    break;
+                }
 
-                ss << '}';
-            } else if (id == ARMInstructionID::B) {
-                /*  */
-                int32_t off = params.branch.offset * 4;
-
-                ss << "B" << (params.branch.l ? "L" : "") << " "
-                   << "PC" << (off < 0 ? '-' : '+') << "0x" << std::hex << std::abs(off);
-            } else if (cat == ARMInstructionCategory::SOFTWARE_INTERRUPT) {
-                ss << instructionIDToString(id) << " " << swi::swiToString(params.software_interrupt.comment >> 16);
-            } else {
-                ss << instructionIDToString(id) << "?";
+                case ARMInstructionCategory::DATA_SWP:
+                case ARMInstructionCategory::SIGN_TRANSF:
+                case ARMInstructionCategory::INVALID_CAT:
+                default: {
+                    ss << "?";
+                    break;
+                }
             }
 
             return ss.str();
