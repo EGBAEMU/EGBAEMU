ac9ceb18b794d86dfdb26b07426443f8eee7d332
Tammo Mürmann <tammo.muermann@stud.tu-darmstadt.de>
2020-09-03 19:30:49 +0200

    migrate keypad

--- src/io/keypad.hpp
+++ src/io/keypad.hpp
@@ -9,10 +9,38 @@ namespace gbaemu
     class Keypad
     {
       private:
+        static const constexpr uint32_t KEYPAD_REG_BASE_ADDR = Memory::IO_REGS_OFFSET + 0x130;
+
         Memory &memory;
 
+        struct KeypadRegister {
+            uint16_t keyStatus;
+            uint16_t keyIRQCnt;
+        } __attribute__((packed)) regs = {0};
+
+        uint8_t read8FromReg(uint32_t offset)
+        {
+            //TODO endianess???
+            return *(offset + reinterpret_cast<uint8_t *>(&regs));
+        }
+        void write8ToReg(uint32_t offset, uint8_t value)
+        {
+            //TODO endianess???
+            *(offset + reinterpret_cast<uint8_t *>(&regs)) = value;
+        }
+
       public:
-        Keypad(Memory &memory) : memory(memory) {}
+        Keypad(Memory &memory) : memory(memory)
+        {
+            memory.ioHandler.registerIOMappedDevice(
+                IO_Mapped(
+                    KEYPAD_REG_BASE_ADDR,
+                    KEYPAD_REG_BASE_ADDR + sizeof(regs),
+                    std::bind(&Keypad::read8FromReg, this, std::placeholders::_1),
+                    std::bind(&Keypad::write8ToReg, this, std::placeholders::_1, std::placeholders::_2),
+                    std::bind(&Keypad::read8FromReg, this, std::placeholders::_1),
+                    std::bind(&Keypad::write8ToReg, this, std::placeholders::_1, std::placeholders::_2)));
+        }
 
         enum KeyInput : uint8_t {
             BUTTON_A = 0,
@@ -27,24 +55,21 @@ namespace gbaemu
             BUTTON_R,
         };
 
-        static const uint32_t KEY_STATUS_REG = 0x04000130;
-        static const uint32_t KEY_INTERRUPT_CNT_REG = 0x04000132;
-
         static const uint8_t KEY_IRQ_EN_OFFSET = 14;
         static const uint8_t KEY_IRQ_COND_OFFSET = 15;
 
         void setKeyInputState(bool released, KeyInput key)
         {
-            uint16_t currentValue = memory.read16(KEY_STATUS_REG, nullptr);
+            uint16_t currentValue = regs.keyStatus;
             currentValue = (currentValue & ~(static_cast<uint16_t>(1) << key)) | (released ? (static_cast<uint16_t>(1) << key) : 0);
-            memory.write16(KEY_STATUS_REG, currentValue, nullptr);
+            regs.keyStatus = currentValue;
             checkIRQConditions(currentValue);
         }
 
       private:
         void checkIRQConditions(uint16_t keyinputReg)
         {
-            uint16_t conditions = memory.read16(KEY_STATUS_REG, nullptr);
+            uint16_t conditions = regs.keyIRQCnt;
             // Are interrupt enabled?
             if (conditions & (static_cast<uint16_t>(1) << KEY_IRQ_EN_OFFSET)) {
                 bool andCond = conditions & (static_cast<uint16_t>(1) << KEY_IRQ_COND_OFFSET);
