ac621c6df3589e837db3af6cd353ca1cff736bc9
Tammo Mürmann <tammo.muermann@stud.tu-darmstadt.de>
2020-09-27 19:00:06 +0200

    fix windows compile

--- src/lcd/coloreffects.hpp
+++ src/lcd/coloreffects.hpp
@@ -1,9 +1,9 @@
 #ifndef COLOREFFECTS_HPP
 #define COLOREFFECTS_HPP
 
-#include <memory>
 #include "defs.hpp"
-
+#include <array>
+#include <memory>
 
 namespace gbaemu::lcd
 {
@@ -11,17 +11,19 @@ namespace gbaemu::lcd
     {
       public:
         typedef std::array<std::shared_ptr<Layer>, 8>::const_iterator iterator;
+
       private:
         BLDCNT::ColorSpecialEffect effect;
         uint32_t eva;
         uint32_t evb;
         uint32_t evy;
+
       public:
-        void load(const LCDIORegs& regs) noexcept;
+        void load(const LCDIORegs &regs) noexcept;
         color_t apply(color_t first, color_t second = TRANSPARENT) const;
         bool secondColorRequired() const;
         BLDCNT::ColorSpecialEffect getEffect() const;
     };
-}
+} // namespace gbaemu::lcd
 
 #endif /* COLOREFFECTS_HPP */
