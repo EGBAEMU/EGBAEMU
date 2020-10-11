2aea2c8931bedf1eaa25da8c79bfd85c51a8bcc0
jsch <jsch@jsch.jsch>
2020-10-11 14:13:10 +0200

    palette implements new format color16_t

--- src/lcd/palette.cpp
+++ src/lcd/palette.cpp
@@ -4,30 +4,21 @@ namespace gbaemu::lcd
 {
     /* LCDColorPalette */
 
-    color_t LCDColorPalette::toR8G8B8(color16_t color)
-    {
-        uint32_t r = static_cast<uint32_t>(color & 0x1F) << 3;
-        uint32_t g = static_cast<uint32_t>((color >> 5) & 0x1F) << 3;
-        uint32_t b = static_cast<uint32_t>((color >> 10) & 0x1F) << 3;
-
-        return 0xFF000000 | (r << 16) | (g << 8) | b;
-    }
-
     void LCDColorPalette::loadPalette(Memory &mem)
     {
         bgPalette = reinterpret_cast<uint16_t *>(mem.bg_obj_ram);
         objPalette = reinterpret_cast<uint16_t *>(mem.bg_obj_ram + 0x200);
     }
 
-    color_t LCDColorPalette::getBgColor(uint32_t index) const
+    color16_t LCDColorPalette::getBgColor(uint32_t index) const
     {
         if (index == 0)
             return TRANSPARENT;
 
-        return toR8G8B8(bgPalette[index]);
+        return bgPalette[index];
     }
 
-    color_t LCDColorPalette::getBgColor(uint32_t paletteNumber, uint32_t index) const
+    color16_t LCDColorPalette::getBgColor(uint32_t paletteNumber, uint32_t index) const
     {
         if (index == 0)
             return TRANSPARENT;
@@ -35,15 +26,15 @@ namespace gbaemu::lcd
         return getBgColor(paletteNumber * 16 + index);
     }
 
-    color_t LCDColorPalette::getObjColor(uint32_t index) const
+    color16_t LCDColorPalette::getObjColor(uint32_t index) const
     {
         if (index == 0)
             return TRANSPARENT;
 
-        return toR8G8B8(objPalette[index]);
+        return objPalette[index];
     }
 
-    color_t LCDColorPalette::getObjColor(uint32_t paletteNumber, uint32_t index) const
+    color16_t LCDColorPalette::getObjColor(uint32_t paletteNumber, uint32_t index) const
     {
         if (index == 0)
             return TRANSPARENT;
@@ -51,24 +42,8 @@ namespace gbaemu::lcd
         return getObjColor(paletteNumber * 16 + index);
     }
 
-    color_t LCDColorPalette::getBackdropColor() const
+    color16_t LCDColorPalette::getBackdropColor() const
     {
-        return toR8G8B8(bgPalette[0]);
-    }
-
-    void LCDColorPalette::drawPalette(int32_t size, color_t *target, int32_t stride)
-    {
-        for (int32_t y = 0; y < size; ++y)
-            for (int32_t x = 0; x < size * 256; ++x) {
-                int32_t index = x / size;
-                color_t color;
-
-                if (index == 0)
-                    color = getBackdropColor();
-                else
-                    color = getBgColor(index);
-
-                target[y * stride + x] = color;
-            }
+        return bgPalette[0];
     }
 } // namespace gbaemu::lcd
--- src/lcd/palette.hpp
+++ src/lcd/palette.hpp
@@ -12,18 +12,16 @@ namespace gbaemu::lcd
         /* 256 entries */
         const color16_t *objPalette;
 
-        static color_t toR8G8B8(color16_t color);
         void loadPalette(Memory &mem);
         /*
             Under certain conditions the palette can be split up into 16 partitions of 16 colors. This is what
             partition number and index refer to.
          */
-        color_t getBgColor(uint32_t index) const;
-        color_t getBgColor(uint32_t paletteNumber, uint32_t index) const;
-        color_t getObjColor(uint32_t index) const;
-        color_t getObjColor(uint32_t paletteNumber, uint32_t index) const;
-        color_t getBackdropColor() const;
-        void drawPalette(int32_t size, color_t *target, int32_t stride);
+        color16_t getBgColor(uint32_t index) const;
+        color16_t getBgColor(uint32_t paletteNumber, uint32_t index) const;
+        color16_t getObjColor(uint32_t index) const;
+        color16_t getObjColor(uint32_t paletteNumber, uint32_t index) const;
+        color16_t getBackdropColor() const;
     };
 } // namespace gbaemu::lcd
 
